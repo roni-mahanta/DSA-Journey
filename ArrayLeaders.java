15e09d6cc33663702bfd162c17ed736f647e0bb1
Ronicoderpro <ronimahanta165@gmail.com>
2026-09-02 11:13:32 +0530

    Add Array Leaders problem

---
+++ ArrayLeaders.java
@@ -0,0 +1,29 @@
+
+public class ArrayLeaders {
+
+    public static void main(String[] args) {
+
+        int[] arr = {16, 17, 4, 3, 5, 2};
+
+        findLeaders(arr);
+    }
+
+    static void findLeaders(int[] arr) {
+
+        int n = arr.length;
+
+        int rightmost = arr[n - 1];
+
+        System.out.print("Leaders: " + rightmost + " ");
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            if (arr[i] > rightmost) {
+
+                rightmost = arr[i];
+
+                System.out.print(rightmost + " ");
+            }
+        }
+    }
+}
