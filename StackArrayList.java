8efac71d7da04c0f25f1763b7a030b7898f16fc1
Ronicoderpro <ronimahanta165@gmail.com>
2026-09-14 22:06:22 +0530

    Stack implemented using array

---
+++ StackArrayList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class StackArrayList {
+    static class Stack{
+       private ArrayList<Integer> list=new ArrayList<>();
+        public  boolean isEmpty(){
+            return list.size()==0;
+        }
+        public void push(int data){
+            list.add(data);
+        }
+        public int pop(){
+            int top= list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+        public  int peek(){
+            if (isEmpty()){
+                return -1;
+            }
+            return list.get(list.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        Stack s=new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
