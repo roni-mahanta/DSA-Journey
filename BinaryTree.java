7732d82c1da2dc44ca6fa4764963403dc7627bf2
Ronicoderpro <ronimahanta165@gmail.com>
2026-09-16 18:32:34 +0530

    Building a binary tree

---
+++ BinaryTree.java
@@ -0,0 +1,40 @@
+public class BinaryTree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class BinaryTrees{
+        static int index=-1;
+        public static Node buildTree(int[] nodes){
+            index++;
+            if (nodes[index]==-1){
+                return null;
+            }
+            Node newNode=new Node(nodes[index]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+            return newNode;
+        }
+    }
+    public static void preOrder(Node root){
+        if (root==null){
+            return;
+        }
+        System.out.print(root.data+ " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public static void main(String[] args) {
+        int[] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTrees tree=new BinaryTrees();
+        Node root=tree.buildTree(nodes);
+        preOrder(root);
+
+    }
+}
