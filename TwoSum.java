89cf5601e5d189255d6fffb690b6e328c95e0142
Ronicoderpro <ronimahanta165@gmail.com>
2026-09-18 10:25:21 +0530

    Two sum solve using hashmap

---
+++ TwoSum.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class TwoSum {
+
+        public static void main(String[] args) {
+            int[] nums= {2,7,8,4};
+            int target=9;
+            HashMap<Integer,Integer> map=new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int need=target - nums[i];
+                if (map.containsKey(need)){
+                    System.out.println(map.get(need) + " " + i);
+                    return;
+                }
+                map.put(nums[i],i);
+            }
+
+        }
+
+
+}
+
